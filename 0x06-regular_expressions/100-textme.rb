bfa086585e1cd74827f427309985b7921fb57452
yonessgt <youness.merzak123@gmail.com>
2023-08-03 17:22:11 +0000

    comment

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-text = "[from:Google] [to:+16474951758] [flags:-1:0:-1:0:-1]"
+# text = "[from:Google] [to:+16474951758] [flags:-1:0:-1:0:-1]"
 puts ARGV[0].scan(/\[from:(\w+|\+\d+)\]\s\[to:(\w+|\+\d+)\]\s\[flags:(.*?)\]/).join(',')
