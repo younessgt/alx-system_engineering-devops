1990b9d5e06fa7f537e6e2fb3481211afcdb4233
yonessgt <youness.merzak123@gmail.com>
2023-09-26 18:44:27 +0000

    fix advanced

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,28 +2,29 @@
 
 include stdlib
 
-exec {'Update':
+exec { 'Update':
 command  => 'sudo apt-get update',
 provider => shell,
 }
 
-exec {'Install-nginx':
+exec { 'Install-nginx':
 command  => 'sudo apt-get install -y nginx',
 provider => shell,
 }
 
-file_line {'hello world':
+file_line { 'hello world':
 ensure => present,
 line   => 'Hello World!',
 path   => '/var/www/html/index.nginx-debian.html',
 }
 
-exec {'301':
-command  => 'sudo sed -i "42i \\\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n" /etc/nginx/sites-available/default',
-provider => present,
+file_line { '301':
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '    location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
 }
 
-exec {'restart server':
+exec { 'restart server':
 command  => 'sudo service nginx restart',
 provider => shell,
 }
