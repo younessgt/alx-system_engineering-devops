609cb5e171f3afe0521997a376571606fc4d4c71
yonessgt <youness.merzak123@gmail.com>
2023-10-02 20:49:55 +0000

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,17 +12,6 @@ command  => 'sudo apt-get install -y nginx',
 provider => shell,
 }
 
-file_line { 'hello world':
-ensure => present,
-line   => 'Hello World!',
-path   => '/var/www/html/index.nginx-debian.html',
-}
-
-file_line { '301':
-  path  => '/etc/nginx/sites-available/default',
-  after => 'listen 80 default_server;',
-  line  => '    location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
-}
 
 exec { 'header':
 
