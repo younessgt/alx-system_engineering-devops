581c51d101d4eaf5b7585a72aa4a8a04427de9ce
yonessgt <youness.merzak123@gmail.com>
2023-10-23 13:57:21 +0000

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,22 +5,23 @@ returns information about his/her TODO list progress """
 import requests
 from sys import argv
 
-response = requests.get("https://jsonplaceholder.typicode.com/users")
-for emp in response.json():
-    if emp.get("id") == int(argv[1]):
-        employe_name = emp.get("name")
+if __name__ == "__main__":
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    for emp in response.json():
+        if emp.get("id") == int(argv[1]):
+            employe_name = emp.get("name")
 
-response2 = requests.get("https://jsonplaceholder.typicode.com/todos")
-total_task = 0
-num_of_task_done = 0
-list_task_title = []
-for todo in response2.json():
-    if todo.get("userId") == int(argv[1]):
-        total_task += 1
-        if todo.get("completed") is True:
-            num_of_task_done += 1
-            list_task_title.append(todo.get("title"))
-print(f"Employee {employe_name} is done with tasks("
-      f"{num_of_task_done}/{total_task}):")
-for title in list_task_title:
-    print(f"\t {title}")
+    response2 = requests.get("https://jsonplaceholder.typicode.com/todos")
+    total_task = 0
+    num_of_task_done = 0
+    list_task_title = []
+    for todo in response2.json():
+        if todo.get("userId") == int(argv[1]):
+            total_task += 1
+            if todo.get("completed") is True:
+                num_of_task_done += 1
+                list_task_title.append(todo.get("title"))
+    print(f"Employee {employe_name} is done with tasks("
+          f"{num_of_task_done}/{total_task}):")
+    for title in list_task_title:
+        print(f"\t {title}")
