6784d90b492188bbd00c937a5470e8a1eb065450
yonessgt <youness.merzak123@gmail.com>
2023-11-08 16:02:08 +0000

    fix puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 exec {'correct_php':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-  path    => '/usr/bin',
-  onlyif  => 'grep -q class-wp-locale.phpp /var/www/html/wp-settings.php',
+  path    => ["/bin", "/usr/bin"],
+  onlyif  => "grep -q class-wp-locale.phpp /var/www/html/wp-settings.php",
 }
