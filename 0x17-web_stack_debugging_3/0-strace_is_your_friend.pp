ab591ec2f6fde3f53803f187f6e70f0ab96fef0c
yonessgt <youness.merzak123@gmail.com>
2023-11-08 16:05:05 +0000

    adding comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
+# Puppet script fixing the issue find in web debugging 3 "replacinng phpp by php"
 exec {'correct_php':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-  path    => ["/bin", "/usr/bin"],
-  onlyif  => "grep -q class-wp-locale.phpp /var/www/html/wp-settings.php",
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'grep -q class-wp-locale.phpp /var/www/html/wp-settings.php',
 }
